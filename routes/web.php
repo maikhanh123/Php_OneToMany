0e8a929b671427fe95a116b37e7d27d55e9c44b4
maikhanh <maikhanh123@gmail.com>
2019-02-28 22:06:41 +0700

    push to git

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,21 +17,25 @@ Route::get('/', function () {
 
 Route::get('/createUser', function () {
    $user = \App\User::create(['name'=>'Tuan', 'email'=>'vantuan@gmail.com', 'password'=>bcrypt('123')]);
-
    return $user;
 });
 
 Route::get('user/{id}/createpost', function ($id){
    $user = \App\User::findOrFail($id);
-   $post = new \App\Post(['title'=>'PHP']);
+//    $post = new \App\Post(['title'=>'Python']);
+
+   $post = new Post();
+   $post->title = 'Python';
 
    return $user->posts()->save($post);
 });
 
 Route::get('post/update', function () {
-   $post = \App\Post::where('id', 2);
+   $post = \App\Post::where('id', 2);   //where('id','=',2)
    return $post->update(['title'=>'Javascript']);
 });
+
+
 
 Route::get('readpost', function () {
    return \App\Post::all();
@@ -40,21 +44,40 @@ Route::get('readpost', function () {
 Route::get('readpost/{id}', function ($id) {
     $post = \App\Post::findOrFail($id);
     return $post;
-    $user = \App\User::findOrFail($id);
-    return $user->posts;
+    // $user = \App\User::findOrFail($id);
+    // return $user->posts;
 });
 
 Route::get('user/{id}/post', function ($id) {
     $user = \App\User::findOrFail($id);
-    return $user->posts;
+
+    // dd($user->posts);
+
+    foreach ($user->posts as $post) {
+        echo $post->title . "<br>";
+    }
+
+    // return $user->posts;
 
 });
 
 Route::get('user/{id}/updatepost', function ($id) {
     $user = \App\User::findOrFail($id);
 //    return $user->posts()->where('id', 2)->update(['title'=>'Javascript Update']);
-    return $user->posts()->whereId(2)->update(['title'=>'Javascript Update']);
+    // return $user->posts()->whereId(2)->update(['title'=>'Javascript Update']);
+    return $user->posts()->where('title','Python')->update(['title'=>'Python Update']);
 
+});
+
+Route::get('user/{id}/postupdate', function($id){
+    $user = \App\User::findOrFail($id);
+    $post = $user->posts->where('title','ASP');
+    // dd($post);
+    foreach ($post as $p) {
+        $p->title = 'Python';
+        $p->save();
+    }
+    // return $post[0];
 });
 
 Route::get('user/{id}/delete', function ($id) {
